c02dba37e999303ad8eb5574205ffeb87af5afb1
pourquoi0506 <125891683+pourquoi0506@users.noreply.github.com>
2023-07-09 23:46:40 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0rM56xXmnna6Id6DM3uNu11pvNg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'iAT8KV_7ugP_-bBPEpcFZJxgMX8cerEd3VBH7o8Ysy4	',
+      useTemplateId: 'iAT8KV_7ugP_-bBPEpcFZJxgMX8cerEd3VBH7o8Ysy4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-01',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'iAT8KV_7ugP_-bBPEpcFZJxgMX8cerEd3VBH7o8Ysy4	',
+  CALLBACK_TEMPLATE_ID: 'iAT8KV_7ugP_-bBPEpcFZJxgMX8cerEd3VBH7o8Ysy4',
 
   CALLBACK_USERS: [
     {
